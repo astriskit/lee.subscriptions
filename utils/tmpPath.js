faf757718dbda52af2a438e190279f563271cf8e
astriskit <harry2office@gmail.com>
2021-05-06 21:05:05 +0530

    fixing vercel deployment

--- utils/tmpPath.js
+++ utils/tmpPath.js
@@ -3,7 +3,7 @@ const { join } = require('path')
 const { copyFile } = require('fs')
 
 const path = join(tmpdir(), 'db.json')
-copyFile('./data/subscriptions.json', path, (err) => {
+copyFile('../data/subscriptions.json', path, (err) => {
     if (err) {
         console.error("Couldn't copy the data-files")
         console.error(err.message)
